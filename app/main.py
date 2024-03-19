7f20f1deb7488b29214b1cb0eb6cf44e4536dd1c
Vaibhav Jha <vaibhavjha010@gmail.com>
2024-03-19 20:11:40 +0000

    Class 3/19

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-# import boto3
+import JSON
+import boto3
 
 app = FastAPI()
 
@@ -17,6 +18,7 @@ app = FastAPI()
 @app.get("/")  # zone apex
 def zone_apex():
     return {"Hello": "World Wide Web"}
+
     
 
 # Endpoints and Methods
@@ -31,7 +33,10 @@ def add_me(number_1: int, number_2: int):
     return {"sum": sum}
 
 # Let's develop a new one:
-
+@app.get("/divide/{number_1}/{number_2}")
+def divide_me(number_1:int, number_2: int):
+    div = number_2 / number_1
+    return {"quotient": div}
 
 ## Parameters
 # Introduce parameter data types and defaults from the Optional library
@@ -86,9 +91,9 @@ def patch_item(item_id: int, item: Item):
 
 
 # Incorporate with boto3: simpler than the `requests` library:
-# @app.get("/aws/s3")
-# def fetch_buckets():
-#     s3 = boto3.client("s3")
-#     response = s3.list_buckets()
-#     buckets = response['Buckets']
-#     return {"buckets": buckets}
+ @app.get("/aws/s3")
+ def fetch_buckets():
+     s3 = boto3.client("s3")
+     response = s3.list_buckets()
+     buckets = response['Buckets']
+     return {"buckets": buckets}
